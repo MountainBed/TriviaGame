a37a5a401c43397db2cb72b9e470ca633d40ff0c
Ross Blair <Ross Blair>
2017-07-14 22:34:49 -0400

    Basic html and css set up, js placeholders in place.

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var questionBank = [
+{question: "Question 00", correct: "correct answer 00", distractor00: "distractor00", distractor01: "distractor01", distractor02: "distractor02", correctgif: "assets/images/win.png", incorrectgif: "assets/images/loss.png"},  
+{question: "Question 01", correct: "correct answer 01", distractor00: "distractor10", distractor01: "distractor11", distractor02: "distractor12", correctgif: "assets/images/win.png", incorrectgif: "assets/images/loss.png"},  
+{question: "Question 02", correct: "correct answer 02", distractor00: "distractor20", distractor01: "distractor21", distractor02: "distractor22", correctgif: "assets/images/win.png", incorrectgif: "assets/images/loss.png"},  
+{question: "Question 03", correct: "correct answer 03", distractor00: "distractor30", distractor01: "distractor31", distractor02: "distractor32", correctgif: "assets/images/win.png", incorrectgif: "assets/images/loss.png"},  
+{question: "Question 04", correct: "correct answer 04", distractor00: "distractor40", distractor01: "distractor41", distractor02: "distractor42", correctgif: "assets/images/win.png", incorrectgif: "assets/images/loss.png"}
+];
+
+console.log(questionBank[0].question);
